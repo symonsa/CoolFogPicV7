3d6e62549cc5a94880661c35af74ef5d5d6f31cc
symonsa <anthony.symons@itsmp.com>
2025-04-28 13:18:11 +1000

    When the zone goes off, wait 2 seconds, and then the dump turns on for <config> seconds, then turns off Added configurable off time for dump zones Added in statistics reporting

--- shutdown_state.c
+++ shutdown_state.c
@@ -1,23 +1,22 @@
 #include "pump_control.h"
 
 void
-shutdown (void)
-{
-  /*
-   * shutdown:  when  RUN goes hi (pump off)  or faults call for shutdown, then
-   *    1/ set main & booster pump o/p lo (off)
-   *    2/ 1 second delay
-   *    3/ set dump solenoid o/p [RB3] hi for time set by the hex switch on i/p RB4-7
-   *    4/ set dump sol o/p lo
-   *    5/ now back in standby mode.
-   */
-  MAIN_PUMP_SET (0);
-  BOOST_PUMP_SET (0);
-  DUMP_SOLENOID_SET (1);
+shutdown(void) {
+    /*
+     * shutdown:  when  RUN goes hi (pump off)  or faults call for shutdown, then
+     *    1/ set main & booster pump o/p lo (off)
+     *    2/ 1 second delay
+     *    3/ set dump solenoid o/p [RB3] hi for time set by the hex switch on i/p RB4-7
+     *    4/ set dump sol o/p lo
+     *    5/ now back in standby mode.
+     */
+    MAIN_PUMP_SET(0);
+    BOOST_PUMP_SET(0);
+    disableZonesDueToFault = true;
 
 
-
-  enable_callback (EventDumpSolenoid, DUMP_SOLENOID_ON_TIME);
+    // wait 2 sec and then turn on dump
+    enableDumpWaitCallback(EventDumpSolenoid);
 
 
 }
