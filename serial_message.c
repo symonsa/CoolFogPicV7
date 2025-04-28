c2d8fad5268be47ab4fa84c8f57c33f8450f029e
symonsa <anthony.symons@itsmp.com>
2025-04-28 13:17:42 +1000

    When the zone goes off, wait 2 seconds, and then the dump turns on for <config> seconds, then turns off Added configurable off time for dump zones Added in statistics reporting

--- serial_message.c
+++ serial_message.c
@@ -7,15 +7,29 @@
 #define ZONE1POS 3
 #define ZONE2POS 4
 #define DUMP_MODE_POS 5
-#define STATUS1_POS 6
-#define STATUS2_POS 7
-#define STATUS3_POS 8
-#define END_MESSAGE_POS 9
-#define SECOND_END_MESSAGE_POS 10
-#define NUL_POS 11
+#define DUMP_TIME_POS 6
+#define STATUS1_POS 7
+#define STATUS2_POS 8
+#define STATUS3_POS 9
+#define CHECKSUM_POS 10
+#define END_MESSAGE_POS 11
+#define SECOND_END_MESSAGE_POS 12
+#define NUL_POS 13
+#define ERROR_STATS_MSG_TYPE 'e'
+#define ERROR_POS_HI 3
+#define ERROR_POS_LO 4
+#define SENT_POS_HI 5
+#define SENT_POS_LO 6
+#define RECV_POS_HI 7
+#define RECV_POS_LO 8
+#define PADDING_POS 9
 static char outputMessage[MAX_MESSAGE];
 //char address = '0';
 __EEPROM_DATA('0', '1', 0, 0, 0, 0, 0, 0);
+
+unsigned char numSerialErrors = 0;
+unsigned char numSerialReceived = 0;
+unsigned char numSerialSent = 0;
 
 unsigned char xtochar(char x) {
     if ((x >= '0')
@@ -45,10 +59,11 @@ void setBaseMessage(char messageType, char msgAddress) {
 
     outputMessage[ADDRESS_POS] = msgAddress;
     outputMessage[MSG_TYPE_POS] = messageType;
-	outputMessage[ZONE1POS] =     chartox(combinedZones >>4);
-    outputMessage[ZONE2POS] =     chartox((combinedZones &15));
-    
+    outputMessage[ZONE1POS] = chartox(combinedZones >> 4);
+    outputMessage[ZONE2POS] = chartox((combinedZones & 15));
+
     outputMessage[DUMP_MODE_POS] = chartox(dump_mode);
+    outputMessage[DUMP_TIME_POS] = chartox(dump_time);
     // now set the status
 
     char v = (fault_flags.value >> 8) &0xf;
@@ -59,7 +74,7 @@ void setBaseMessage(char messageType, char msgAddress) {
 
     v = (fault_flags.value) &0xf;
     outputMessage[STATUS3_POS] = chartox(v);
-
+    outputMessage[CHECKSUM_POS] = chartox(calculateChecksum(outputMessage));
     outputMessage[END_MESSAGE_POS] = frameEnd;
     outputMessage[SECOND_END_MESSAGE_POS] = frameEnd; // a bit of a hack because the transmit control is going low too early
     outputMessage[NUL_POS] = 0;
@@ -68,6 +83,7 @@ void setBaseMessage(char messageType, char msgAddress) {
 void process_set_zones_message(char * inmessage) {
     commsZones = (xtochar(inmessage[ZONE1POS]) << 4) + xtochar(inmessage[ZONE2POS]);
     dump_mode = xtochar(inmessage[DUMP_MODE_POS]);
+    dump_time = xtochar(inmessage[DUMP_TIME_POS]);
 
     combineZones();
     setBaseMessage('s', inmessage[ADDRESS_POS]);
@@ -86,6 +102,30 @@ void process_get_status_message(char * inmessage) {
     //    for (int i = 0; i < MAX_MESSAGE; i ++){
     //	    printf("msg %c %c\r\n",outputMessage[i],outputMessage[i]+'0' );
     //	} 
+    output_message(outputMessage);
+
+}
+
+void process_error_stats_message(char msgAddress) {
+    outputMessage[0] = frameStart;
+
+    outputMessage[ADDRESS_POS] = msgAddress;
+    outputMessage[MSG_TYPE_POS] = ERROR_STATS_MSG_TYPE;
+
+    outputMessage[ERROR_POS_HI] = chartox(numSerialErrors >> 4);
+    outputMessage[ERROR_POS_LO] = chartox((numSerialErrors & 15));
+
+    outputMessage[SENT_POS_HI] = chartox(numSerialSent >> 4);
+    outputMessage[SENT_POS_LO] = chartox((numSerialSent & 15));
+
+    outputMessage[RECV_POS_HI] = chartox(numSerialReceived >> 4);
+    outputMessage[RECV_POS_LO] = chartox((numSerialReceived & 15));
+    outputMessage[PADDING_POS] = '0';
+    outputMessage[CHECKSUM_POS] = chartox(calculateChecksum(outputMessage));
+
+    outputMessage[END_MESSAGE_POS] = frameEnd;
+    outputMessage[SECOND_END_MESSAGE_POS] = frameEnd; // a bit of a hack because the transmit control is going low too early
+    outputMessage[NUL_POS] = 0;
     output_message(outputMessage);
 
 }
@@ -115,6 +155,13 @@ process_message(char *inmessage) {
 
     //printf("m %c a %c\r\n",inmessage[ADDRESS_POS],address +'0');
     if (inmessage[ADDRESS_POS] == '0' || inmessage[ADDRESS_POS] == address) {
+        unsigned char checkSumCalculated = calculateChecksum(inmessage);
+        unsigned char checkSumActual = xtochar(inmessage[CHECKSUM_POS]);
+        if (checkSumCalculated != checkSumActual) {
+            numSerialErrors++;
+            return;
+        }
+        numSerialReceived++;
 
         msgType = inmessage[MSG_TYPE_POS];
 
@@ -145,12 +192,37 @@ process_message(char *inmessage) {
             case 'm':
                 process_reset_pump(inmessage);
                 break;
+            case 'E':
+            case 'e':
+                process_error_stats_message(inmessage[ADDRESS_POS]);
+                break;
 
+            default:
+                numSerialErrors++;
 
         }
 
 
     }
+}
+
+// Function to calculate the 4-bit checksum of a string
+
+unsigned char calculateChecksum(const char* str) {
+    int sum = 0;
+
+
+    // Iterate through each character in the string
+    for (unsigned char i = ADDRESS_POS; i < CHECKSUM_POS; i++) {
+        sum += (unsigned char) str[i]; // Add the ASCII value of the character to the sum
+    }
+
+    // Fold the sum to 4 bits
+    while (sum > 0xF) { // 0xF is 1111 in binary, the maximum value of a 4-bit number
+        sum = (sum & 0xF) + (sum >> 4);
+    }
+
+    return (unsigned char) (sum & 0xF); // the sum, masked to 4 bits
 }
 
 
